e80a364ced2fa3224087dcf645eb73f2a066c620
rishikant42 <rksbtp@gmail.com>
2016-10-19 19:35:47 +0530

    added ex 8 & 9

---
+++ ex9/1.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "jan\nfeb\nmar\napr\nmay\jun\njul\naug"
+print "Here are the days:", days
+print "Here are the months:",months
+
+print """
+There's something going on here.
+by using three double-quotes.
+we will able type many line, we req.
+like
+my name  rishi
