2b47fdd6352d3b9072939b483a2efece5bca3946
rishikant <rksbtp@gmail.com>
2017-04-21 21:45:22 +0530

    recursive & iterative approaches

---
+++ others/13.py
@@ -0,0 +1,16 @@
+# recursive approach
+def fact1(n):
+    if n == 1:
+        return 1
+    else:
+        return n * fact1(n-1)
+
+# iterative approach
+def fact2(n):
+    res = 1
+    for i in range(1,n+1):
+        res *= i
+    return res
+
+print fact1(6)
+print fact2(6)
