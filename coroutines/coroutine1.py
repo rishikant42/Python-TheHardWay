0d54edbe34fe56c5204bce263ecdddcd47a3f828
rishikant42 <rksbtp@gmail.com>
2018-06-26 10:37:09 +0530

    simple coroutine

---
+++ coroutines/coroutine1.py
@@ -0,0 +1,18 @@
+def print_name(prefix):
+    print("Searching prefix:{}".format(prefix))
+    while True:
+        name = (yield)
+        if prefix in name:
+            print(name)
+
+# calling coroutine, nothing will happen
+corou = print_name("Dear")
+
+# This will start execution of coroutine and 
+# Prints first line "Searchig prefix..."
+# and advance execution to the first yield expression
+corou.next()
+
+# sending inputs
+corou.send("Atul")
+corou.send("Dear Atul")
